3ab44389ed5837fc2f57d52444786895cf1bf341
jer0dh <jerod@jhtechservices.com>
2016-08-17 13:37:17 -0500

    removing require statement for psw.json

--- data/ideaDb.js
+++ data/ideaDb.js
@@ -1,6 +1,6 @@
 var MongoClient = require("mongodb").MongoClient;
 var mongoose = require('mongoose');
-var pws = require('../pws.json');
+//var pws = require('../pws.json');
 
 // var url = 'mongodb://' + pws.mongodbUser + ':'+ pws.mongodbPw +'@' + pws.mongodbUrl;
 var url = 'mongodb://' + process.env.mongodbUser + ':'+ process.env.mongodbPw +'@' + process.env.mongodbUrl;
