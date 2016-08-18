c9bc2bd1ce804c8a6e4088ad5893c14496a3832a
Mike Arnold <dharmamike@gmail.com>
2016-08-18 06:46:04 -0500

    Add dotenv for environment configuration

--- data/ideaDb.js
+++ data/ideaDb.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 //var pws = require('../pws.json');
 
 // var url = 'mongodb://' + pws.mongodbUser + ':'+ pws.mongodbPw +'@' + pws.mongodbUrl;
-var url = 'mongodb://' + process.env.mongodbUser + ':'+ process.env.mongodbPw +'@' + process.env.mongodbUrl;
+var url = 'mongodb://' + process.env.MONGODB_USER + ':'+ process.env.MONGODB_PASS +'@' + process.env.MONGODB_URL;
 // var connect = MongoClient.connect(url);
 
 /* change mongoose promise to a true promise implementation */
